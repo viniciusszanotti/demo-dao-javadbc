a0f7442b786dbf6cf2c55734813a174cd118bd39
Vinicius Zanotti <viniciusszanotti@gmail.com>
2023-03-26 09:09:22 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -91,6 +94,50 @@ public class SellerDaoJDBC implements SellerDao{
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName "
+					+ "FROM seller INNER JOIN department "
+					+ "ON seller.DepartmentId = department.Id "
+					+ "WHERE DepartmentId = ? "
+					+ "ORDER BY Name "
+					);
+			
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();//executa o comando e cai dentro do rs
+			
+			List<Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();//guardar dentro desse map qualquer departamento que instanciar
+			
+			while (rs.next()) { //usado o while pois há mais de um valor
+				//a cada vez q passar pelo while, será testado se o departamento já existe, conforme comando abaixo.
+				Department dep = map.get(rs.getInt("DepartmentId"));
+				
+				if (dep == null) { //caso seja um departamento qua ainda não exista...ele irá instanciar e salvar no map esse novo dep
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+				
+			}
+			return list;
+		}
+		catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			
+		}
 	}
 
 }
