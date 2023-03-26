8cab8693b8767f69c0a7cf4c942a9bdc1d12fef3
Vinicius Zanotti <viniciusszanotti@gmail.com>
2023-03-26 09:24:38 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -29,7 +30,12 @@ public class Program {
 		for (Seller obj: list) {
 			System.out.println(obj);
 		}
-
+		
+		System.out.println("\n=== TEST 4: seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);//inserir no banco de dados o novo vendedor
+		System.out.println("Inserted! New id = " + newSeller.getId());
+		
 	}
 
 }
